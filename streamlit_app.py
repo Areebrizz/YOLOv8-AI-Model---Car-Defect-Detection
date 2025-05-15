19ce53b251da30ddb93be0cceb7e860b11538a73
Areeb Rizz <engr.areebriz@gmail.com>
2025-05-15 15:24:26 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,8 @@ uploaded_file = st.file_uploader("📤 Upload a car image", type=["jpg", "jpeg",
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
         image.save(tmp.name)
